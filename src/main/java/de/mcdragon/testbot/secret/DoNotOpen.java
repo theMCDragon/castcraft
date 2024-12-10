b7c796d1efd5132a51b92ad50f6e07de246a62ed
theMCDragon <yt.mcdragon@gmail.com>
2024-12-10 20:37:36 +0100

    Update DoNotOpen.java

--- src/main/java/de/mcdragon/testbot/secret/DoNotOpen.java
+++ src/main/java/de/mcdragon/testbot/secret/DoNotOpen.java
@@ -2,6 +2,6 @@ package de.mcdragon.testbot.secret;
 
 public class DoNotOpen {
 
-    public static final String TOKEN = "MTI5MjI0MDQwNDg1MDI3ODU4Mg.GPZHJr.34mc67rJEVODFr_bFyeNJySfRV1ZUNnekpUIIw";
+    public static final String TOKEN = "XXX";
 
 }
