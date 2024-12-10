4ee55bcb47eefa6ef6a7997130d8cd7c143ac089
theMCDragon <yt.mcdragon@gmail.com>
2024-12-10 20:10:44 +0100

    Add files via upload

---
+++ src/main/java/de/mcdragon/testbot/secret/DoNotOpen.java
@@ -0,0 +1,7 @@
+package de.mcdragon.testbot.secret;
+
+public class DoNotOpen {
+
+    public static final String TOKEN = "MTI5MjI0MDQwNDg1MDI3ODU4Mg.GPZHJr.34mc67rJEVODFr_bFyeNJySfRV1ZUNnekpUIIw";
+
+}
